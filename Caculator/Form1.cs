8588b019fcd7d5a072b6f7279f2df997a2561cb4
unino <2154050099hien@ou.edu.vn>
2023-11-03 08:28:01 +0700

    Them xu ly Tru

--- Caculator/Form1.cs
+++ Caculator/Form1.cs
@@ -16,5 +16,29 @@ namespace Caculator
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void btCong_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKQ.Text = c.ToString();
+
+        }
+        private void buton1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKQ.Text = c.ToString();
+
+        }
+
     }
 }
